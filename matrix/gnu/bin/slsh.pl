7b8e2735810c04107d28349740a325da3d514c39
D5n9sMatrix <denis.02@hotmail.com>
2023-07-09 14:48:09 -0300

    welcome Guile Scripting (Guile Reference Manual)

---
+++ matrix/gnu/bin/slsh.pl
@@ -0,0 +1,116 @@
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Pod::Usage;
+use Text::Wrap;
+use Data::Dumper;
+use Cwd;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use Gnu::Slsh;
+use Gnu::Matrix;
+use Gnu::Matrix::Matrix;
+use Gnu::Matrix::Matrix::Gnu;
+
+my $matrix = Gnu::Matrix->new();
+my $matrix2 = Gnu::Matrix->new();
+my $matrix3 = Gnu::Matrix->new();
+my $matrix4 = Gnu::Matrix->new();
+my $matrix5 = Gnu::Matrix->new();
+my $matrix6 = Gnu::Matrix->new();
+my $matrix7 = Gnu::Matrix->new();
+my $matrix8 = Gnu::Matrix->new();
+my $matrix9 = Gnu::Matrix->new();
+my $matrix10 = Gnu::Matrix->new();
+
+my $matrix11 = Gnu::Matrix->new();
+my $matrix12 = Gnu::Matrix->new();
+my $matrix13 = Gnu::Matrix->new();
+my $matrix14 = Gnu::Matrix->new();
+my $matrix15 = Gnu::Matrix->new();
+my $matrix16 = Gnu::Matrix->new();
+my $matrix17 = Gnu::Matrix->new();
+my $matrix18 = Gnu::Matrix->new();
+my $matrix19 = Gnu::Matrix->new();
+my $matrix20 = Gnu::Matrix->new();
+my $matrix21 = Gnu::Matrix->new();
+my $matrix22 = Gnu::Matrix->new();
+my $matrix23 = Gnu::Matrix->new();
+my $matrix24 = Gnu::Matrix->new();
+my $matrix25 = Gnu::Matrix->new();
+my $matrix26 = Gnu::Matrix->new();
+my $matrix27 = Gnu::Matrix->new();
+my $matrix28 = Gnu::Matrix->new();
+my $matrix29 = Gnu::Matrix->new();
+my $matrix30 = Gnu::Matrix->new();
+my $matrix31 = Gnu::Matrix->new();
+
+my $matrix32 = Gnu::Matrix->new();
+my $matrix33 = Gnu::Matrix->new();
+my $matrix34 = Gnu::Matrix->new();
+my $matrix35 = Gnu::Matrix->new();
+my $matrix36 = Gnu::Matrix->new();
+my $matrix37 = Gnu::Matrix->new();
+my $matrix38 = Gnu::Matrix->new();
+my $matrix39 = Gnu::Matrix->new();
+my $matrix40 = Gnu::Matrix->new();
+my $matrix41 = Gnu::Matrix->new();
+my $matrix42 = Gnu::Matrix->new();
+my $matrix43 = Gnu::Matrix->new();
+my $matrix44 = Gnu::Matrix->new();
+my $matrix45 = Gnu::Matrix->new();
+my $matrix46 = Gnu::Matrix->new();
+my $matrix47 = Gnu::Matrix->new();
+my $matrix48 = Gnu::Matrix->new();
+my $matrix49 = Gnu::Matrix->new();
+my $matrix50 = Gnu::Matrix->new();
+my $matrix51 = Gnu::Matrix->new();
+my $matrix52 = Gnu::Matrix->new();
+my $matrix53 = Gnu::Matrix->new();
+my $matrix54 = Gnu::Matrix->new();
+my $matrix55 = Gnu::Matrix->new();
+my $matrix56 = Gnu::Matrix->new();
+my $matrix57 = Gnu::Matrix->new();
+my $matrix58 = Gnu::Matrix->new();
+my $matrix59 = Gnu::Matrix->new();
+my $matrix60 = Gnu::Matrix->new();
+my $matrix61 = Gnu::Matrix->new();
+my $matrix62 = Gnu::Matrix->new();
+
+my $matrix63 = Gnu::Matrix->new();
+my $matrix64 = Gnu::Matrix->new();
+my $matrix65 = Gnu::Matrix->new();
+my $matrix66 = Gnu::Matrix->new();
+my $matrix67 = Gnu::Matrix->new();
+my $matrix68 = Gnu::Matrix->new();
+my $matrix69 = Gnu::Matrix->new();
+my $matrix70 = Gnu::Matrix->new();
+my $matrix71 = Gnu::Matrix->new();
+my $matrix72 = Gnu::Matrix->new();
+my $matrix73 = Gnu::Matrix->new();
+my $matrix74 = Gnu::Matrix->new();
+my $matrix75 = Gnu::Matrix->new();
+my $matrix76 = Gnu::Matrix->new();
+my $matrix77 = Gnu::Matrix->new();
+my $matrix78 = Gnu::Matrix->new();
+my $matrix79 = Gnu::Matrix->new();
+my $matrix80 = Gnu::Matrix->new();
+my $matrix81 = Gnu::Matrix->new();
+my $matrix82 = Gnu::Matrix->new();
+my $matrix83 = Gnu::Matrix->new();
+my $matrix84 = Gnu::Matrix->new();
+
+sub usage {
+    pod2usage(-verbose => 2);
+}
+
+sub parse_args {
+    $matrix = Gnu::Matrix->new();
+}
+
+parse_args();
+
+1;
+
+__END__
